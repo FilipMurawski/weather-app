fde904d5529bc1fe53412e8083099c51a5ec447e
Filip Murawski <filmur98@wp.pl>
2023-04-01 16:25:57 +0200

    Added API request and styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,34 @@
 let weatherForm = document.querySelector('.weather__form')
 let inputCity = document.querySelector('.weather__city')
+let apiDataContainer = document.querySelector('.weather__data')
 
+let apiUrl = 'https://api.weatherapi.com/v1/current.json?key=1aa3e20430224597beb131225230104&aqi=yes&q='
 
 weatherForm.addEventListener('submit', (event) => {
     let city = inputCity.value;
-    console.log(city)
+    let fullApiUrl = apiUrl + city
+
+    fetch(fullApiUrl).then(response => response.json()).then((dataFromApi) => {
+        // console.log(dataFromApi.current.temp_c)
+        let view = ``
+        // view += `W ${dataFromApi.location.name} jest dzisiaj ${dataFromApi.current.temp_c} stopni Celcjusza`       
+        view += `<div class="weather__info">`
+
+        // icon
+        view += `<div class="weather__icon"><img src="${dataFromApi.current.condition.icon}" alt="${dataFromApi.current.condition.text}"></div>`
+        // temperature
+        view += `<div class="weather__temp">${dataFromApi.current.temp_c}<span><sup>o</sup>C</span></div>`
+        // special data
+        view += `<div class="weather__additional-info">`
+        // humidity
+        view += `<div class="weather__humidity">Humidity: ${dataFromApi.current.humidity}%</div>`
+        // wind
+        view += `<div class="weather__wind">Wind: ${dataFromApi.current.wind_kph} km/h</div>`
+
+        view += `</div>`
+        view += `</div>`
+        apiDataContainer.innerHTML = view
+    })
+
     event.preventDefault()
 })
